5ed791aa82162af7bd89c6b25665b5bb3bb1d8a9
bigman16 <therealbl4nc@gmail.com>
2022-03-31 09:39:51 +1100

    New "Sell" command

--- commands/sell.js
+++ commands/sell.js
@@ -11,64 +11,123 @@ module.exports = {
         "sell max rock shard",
         "sell half feather"
     ],
-    execute(message, args, Discord, user) {
-        const sellEmbed = new Discord.MessageEmbed()
-        .setColor(botColour) 
-        .setTimestamp()
-        .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.avatarURL());
-        
+    execute(message, args, Discord, f, user) {
 
         var item = message.content.split(prefix + "sell ").pop();
-        var amount = 1;
+        var amount = item.split(" ")[0];
+        var amountInt = parseInt(amount, 10); // the 10 at the end refers to the radix (the base), base 10 is what we count in
 
         // if Amount is specified (through an integer)
-        // This does mean items names cant start with a number would cause errors
-        if (parseInt(item[0])) {
-            amount = item.split(" ");
-            amount = parseInt(amount[0]);
+        // ------- This does mean items names cant start with a number would cause errors -------
+        var half = false
+        var max = false
+        if (isNaN(amountInt)) {
+            console.log(amount)
 
+            if (amount === "all" || amount === "max") {
+                max = true; 
+                item = item.split(amount + " ").pop();
+            }
+            else if (amount === "half") {
+                half = true;
+                item = item.split(amount + " ").pop();
+            }
+        
+            amount = 1;
+        }
+        else {
             if (amount < 1) {
                 message.reply("that's not a valid amount!");
+                return;
             }
             
             item = item.split(amount + " ").pop();
         }
 
         // set all elements to lower case so command isnt case sensitive
+        item = item.split(" ")
         for (var i = 0; i < item.length; i++) { 
             item[i] = item[i].toLowerCase();
         }
         
-        itemPos = undefined;
-        for (var i = 0; i < user.inventory.length; i++) {
-            var invItem = user.inventory[i].name.split(" ");
+        let itemPos = undefined;
+        for (let i = 0; i < user.inventory.length; i++) {
+            var invItem = user.inventory[i].name.split(" "); 
 
-            var loopLength = Math.max(invItem.length, item.length); // use as the loop amount for the nested loop below
+            let loopLength = Math.max(invItem.length, item.length); // use as the loop amount for the nested loop below
 
-            for (var j = 0; j < loopLength; j++) {
-                // If the words that make up an item name dont match 
-                // at any point, break
-                if (invItem[j].toLowerCase() != item[j]) {
+            for (let j = 0; j < loopLength; j++) {
+                // console.log("ItemPos: " + itemPos)
+                // console.log("Item: " + item)
+                // console.log("InvItem: " + invItem)
+                // console.log("LoopLength: " + loopLength)
+                // console.log("Index: " + j)
+
+                if (item[j] === undefined || invItem[j] === undefined) {
                     break;
                 }
 
-                else if (invItem[j] == item[j] && j == loopLength - 1) {
+                // If the words that make up an item name dont match 
+                // at any point, not the correct item
+                if (invItem[j].toLowerCase() != item[j].toLowerCase()) {
+                    break;
+                }
+                   
+                // found item by getting to end of J without breaking
+                if (j === loopLength - 1) {
                     itemPos = i;
                 }
             }
         }
 
+        // item
+        let foundItem = user.inventory[itemPos]
 
+        // cant find item
         if (itemPos === undefined) {
-            message.reply("that's not a valid item!\nMake sure you spelt it correctly!");
+            message.reply("that's not a valid item!\nMake sure you've spelt it correctly!");
             return;
         }
 
+        // if given amount is too much
         if (amount > user.inventory[itemPos].amount) {
-            message.reply("You don't have that many " + user.inventory[itemPos].name + 
-            "\nYou only have `x" + user.inventory[itemPos].amount + "`");
+            message.reply("You don't have that many " + user.inventory[itemPos].name + "'s" + 
+            "\nYou only have `x" + user.inventory[itemPos].amount + " " + user.inventory[itemPos].name + "'s`");
             return;
         }
+
+
+        // Item has been found, sell process --------------------
+        if (half) { amount = Math.round(foundItem.amount / 2); }
+        else if (max) { amount = foundItem.amount; }
+
+        let earnings = foundItem.sellValue * amount;
+        user.balance += earnings;
+        foundItem.amount -= amount;
+        
+        // remove from inventory if run out
+        if (foundItem.amount === 0) user.inventory.splice(itemPos, 1); 
+
+        // embed
+        const sellEmbed = new Discord.MessageEmbed()
+        .setColor(botColour) 
+        .setTimestamp()
+        .setTitle("Item Sold!")
+        .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.avatarURL());
+
+        // grammar :)
+        if (amount > 1) {
+            sellEmbed.setDescription("You've sold `x" + amount + " " + foundItem.name + "'s` " +
+            "for `$" + earnings + "`")
+        }
+        else {
+            sellEmbed.setDescription("You've sold `x" + amount + " " + foundItem.name + "` " +
+            "for `$" + earnings + "`")
+        }
+
+        f.updateUser()
+
+        message.channel.send(sellEmbed);
     }
 }
 
